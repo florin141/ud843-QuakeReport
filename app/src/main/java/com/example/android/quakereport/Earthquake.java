44b3b3d9abb6f3eca7b1768349afc4ad32890bd4
Florin CIOBANU <29834996+florin141@users.noreply.github.com>
2020-06-07 00:48:42 +0200

    Starting point

---
+++ app/src/main/java/com/example/android/quakereport/Earthquake.java
@@ -0,0 +1,39 @@
+package com.example.android.quakereport;
+
+import java.util.Date;
+
+public class Earthquake {
+    private String magnitude;
+    private String location;
+    private String date;
+
+    public Earthquake(String magnitude, String location, String date) {
+        this.magnitude = magnitude;
+        this.location = location;
+        this.date = date;
+    }
+
+    public String getMagnitude() {
+        return magnitude;
+    }
+
+    public void setMagnitude(String magnitude) {
+        this.magnitude = magnitude;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
